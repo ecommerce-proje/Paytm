5998b63ec99e0208a1f717a60d1411c4369d7a97
Jagadish Alladi <jagadish_a@hclaig.com>
2019-06-27 10:10:29 +0530

    Skipping the staging step

--- login2.java
+++ login2.java
@@ -4,4 +4,8 @@ sample jav program for thenew file login2ava
 O am adding these lines in next day
 for checking purpose
 fine
+I have added some more lines related to the onn27/06
+
+and trying to update the same
+
 
